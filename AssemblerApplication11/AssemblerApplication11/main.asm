a7cbef50b0ec6031621b29bc8a2390c08bef8704
KhaiTTNguyen <tuh23333@temple.edu>
2021-02-10 21:21:04 -0500

    lab 3 brnaching done

--- AssemblerApplication11/AssemblerApplication11/main.asm
+++ AssemblerApplication11/AssemblerApplication11/main.asm
@@ -4,6 +4,42 @@
 ; Created: 2/10/2021 3:30:53 PM
 ; Author : KhaiNguyen
 ;
+/*
+// Activity 1-1s
+LDI R16, 0xa1	; load 0xa1 to r16
+LDI R17, 0x08	; load 0x08 to r17
+LDI R18, 0x9c	; load 0x9c to r18
+LDI R19, 0x14	; load 0x14 to r19
+LDI R20, 0xf7	; load 0xf7 to 20
+
+ADD R16, R17	; add R17 to R16
+ADD R16, R18	; add R18 to R16
+ADD R16, R19	; add R19 to R16
+ADD R16, R20	; add R20 to R16
+*/
+
+// Activity 1-2
+	BLOCK1:
+	LDI	R20,$27
+	LDI	R21,$15
+	SUB	R20, R21 ;Look at the Status Register and check by hand
+	
+	BLOCK2:
+	LDI	R20,$20
+	LDI	R21,$15
+	SUB	R20, R21 ;Look at the Status Register and check by hand 
+ 	
+	BLOCK3:
+	LDI	R24,95
+	LDI	R25,95
+	SUB	R24, R25 ;Look at the Status Register and check by hand
+	
+	BLOCK4:
+	LDI	R22,50
+	LDI	R23,70
+	SUB	R22, R23 ;Look at the Status Register and check by hand
+
+L1:	RJMP	L1 ;Stay here forever
 
 
 // Activity 3-1
@@ -21,7 +57,8 @@ and clear all memory values when you read a new number.
 Follow the flowchart3-1 below. Add a video result of running the code for all cases.  
 */
 
-ldi r18, 10 ; for 4 loading times
+/*
+ldi r18, 10 ; for 10 loading times
 load: 
 	LDI R16, 0	; clear mem
 	IN R16, PINB ; read 0 to 3 
@@ -55,6 +92,37 @@ load:
 		dec r18			; decrement r18
 		brne load		;go back to "load" if r18 is not 0
 nop
+
+*/
+
+// Activity 3-2
+/*
+This assembly code loads value $10 to R20. 
+Decrement the value by 1 until the value becomes zero. 
+Output each value through PORTA if the number becomes an odd number 
+and through PORTB if the number is an even number. 
+Follow the flowchart 3-2.
+*/
+
+/*
+LDI R20, 0x10			; load 0x10 - cannot be 0x00
+LDI R18, 0				; clear mem
+LDI R17, 0b0000001		; masking-register
+LOOP:
+	MOV R18, R20
+	AND R18, R17
+	brne odd			; Z bit not on - odd num
+	
+	OUT PORTB, R20		; even num - out to portB
+	DEC R20				; decrement R20
+	brne LOOP			; back to loop
+
+	odd: 
+		OUT PORTA, R20	; odd num - out to portA
+		DEC R20			; decrement R20
+		brne LOOP		; back to loop
+nop
+*/
 
 /*
 //Activity 3-3. masking
