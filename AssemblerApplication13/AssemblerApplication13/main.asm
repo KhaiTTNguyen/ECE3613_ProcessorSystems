327cc020fad62a9eccef65013756a8e3893543ad
KhaiTTNguyen <tuh23333@temple.edu>
2021-02-15 11:33:46 -0500

    LED toggle, stack pushing

---
+++ AssemblerApplication13/AssemblerApplication13/main.asm
@@ -0,0 +1,61 @@
+;
+; AssemblerApplication13.asm
+;
+; Created: 2/15/2021 11:19:40 AM
+; Author : KhaiNguyen
+;
+
+/*
+LDI R16,HIGH(RAMEND)
+OUT SPH,R16
+LDI R16,LOW(RAMEND)
+OUT SPL,R16        
+LDI R20,15
+LDI R21,5
+CALL FUNC_NAME
+INC  R20
+L1:     RJMP  L1
+FUNC_NAME:
+        ADD  R20,R21
+        SUBI  R20,3
+CALL SUB_1
+NOP
+        RET
+SUB_1:
+PUSH R20
+PUSH R21
+POP R21
+POP R20
+RET
+*/
+
+.ORG 0
+LDI R16,HIGH(RAMEND)  ;load SPH
+OUT SPH,R16
+LDI R16,LOW(RAMEND)  ;load SPL
+OUT SPL,R16
+LDI  R20,0xFF ;R20 = 0xFF
+OUT  DDRA,R20  ;DDRA = R20 (Port B output)
+BACK:
+LDI  R16,0x55	;load R16 with 0x55 - lightup
+OUT  PORTA,R16  ;send 55H to port A
+CALL DELAY		;time delay
+LDI  R16,0xAA	;load R16 with 0xAA
+OUT  PORTA,R16  ;send 0xAA to port A
+CALL DELAY		;time delay
+RJMP BACK		;keep doing this indefinitely
+
+
+DELAY:
+ ldi r20,32
+L1: LDI R21, 200
+L2: LDI R22, 250
+L3: NOP
+NOP
+DEC R22
+BRNE L3
+DEC R21
+BRNE L2
+DEC R20
+BRNE L1
+RET
