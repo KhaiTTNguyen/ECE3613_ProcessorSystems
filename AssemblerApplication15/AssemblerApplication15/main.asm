78bdd6a895e682c99df1d8e81e90fbf81704786d
KhaiTTNguyen <tuh23333@temple.edu>
2021-02-17 15:55:42 -0500

    I/O led blink

--- AssemblerApplication15/AssemblerApplication15/main.asm
+++ AssemblerApplication15/AssemblerApplication15/main.asm
@@ -5,7 +5,7 @@
 ; Author : KhaiNguyen
 ;
 
-
+/*
 //LED 0 Blinking
 LDI R16,HIGH(RAMEND)
 OUT SPH,R16
@@ -36,3 +36,40 @@ DELAY: LDI R20,32			; 64 for 1 sec
 		DEC R20
 		BRNE L1
 	RET
+
+	*/
+
+ldi r16, 0x00
+out ddrb,r16		; make portb an input --> need to actiate pullup resistors
+ldi r16, 0xFF
+out ddra, r16		; make porta an output
+out portb, r16		; activate portb pullup resistors
+ldi r16, 0x00
+ldi r17, 0x01
+ldi r18, 0x02
+ldi r19, 0x03
+;.... r20-24 for 0x04 - 0x07
+mainloop:
+	sbis pinb,0				; skip if bit 0 is set
+	out porta, r16			; send out 
+
+	sbis pinb,1				; skip if switch is opened / bit 1 is set
+	out porta, r17			; send a "1"
+	sbic pinb, 1			; skip if swithc open
+	out porta, r16
+
+	sbis pinb,2				; skip if switch is opened / bit 1 is set
+	out porta, r18			; send a "1"
+	sbic pinb, 2			; skip if swithc open
+	out porta, r16
+
+
+	sbis pinb,3 			; skip if switch is opened / bit 1 is set
+	out porta, r19			; send a "1"
+	sbic pinb, 3			; skip if swithc open
+	out porta, r16
+
+	rjmp mainloop
+
+
+
