cd989cc013fed0a1223130317c296700db01dc5b
KhaiTTNguyen <tuh23333@temple.edu>
2021-02-17 11:58:49 -0500

    I/O programming set port as output/input

--- AssemblerApplication14/AssemblerApplication14/main.asm
+++ AssemblerApplication14/AssemblerApplication14/main.asm
@@ -5,6 +5,7 @@
 ; Author : KhaiNguyen
 ;
 
+/*
 ldi r16, 0xFF
 out ddra,r16			; set porta as output
 out ddrd,r16			; set portd as output
@@ -16,4 +17,19 @@ out ddrd, r18			; make portd input
 L1:
 	in r16, pind		; read in from D
 	out porta, r16		; output to A
-	rjump L1
+	rjmp L1
+
+	// once portD output, input pin also got same value (due to circuit config)
+	*/
+
+ldi r16, 0xFF
+out ddra, r16			; make portA input
+out portb, r16			; activate all pullups resistors on//close switches
+
+ldi r16, 0x00			
+out ddrb, r16			; make B input port
+L2: 
+	in r16, pinb		; input from pinB	// use PIN --> must firist activate pull up resistors
+	out porta, r16		; send to porta LEDs	-- > output also send same data to input
+	rjmp L2
+
