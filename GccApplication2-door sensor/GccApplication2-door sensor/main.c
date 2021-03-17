a23eecdb5248be65f5f186b972b04a56acc52091
KhaiTTNguyen <tuh23333@temple.edu>
2021-03-17 11:56:38 -0400

    I/O programming door sensor

--- GccApplication2-door sensor/GccApplication2-door sensor/main.c
+++ GccApplication2-door sensor/GccApplication2-door sensor/main.c
@@ -8,10 +8,9 @@
 #include <avr/io.h>
 #define LED 7
 #define SENSOR 3
-
+/*
 int main(void)
 {
-    /* Replace with your application code */
     DDRB=(0<<SENSOR);			 // make pb3 input bit on portb
 	DDRA=(1<<LED);					// make pa7 output bit on porta
 	PORTB=0xFF;					// pullups
@@ -25,5 +24,23 @@ int main(void)
 			PORTA=(0<<LED);			// turn off PA7
 		}
     }
+}
+*/
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRB=DDRB& ~(1<<SENSOR);				// make pb3 input bit & preserve other bits
+	DDRA=DDRA | (1<<LED);					// make pa7 output bit 
+	PORTB=0xFF;								// pullups
+	while (1)
+	{
+		if(PINB&(1<<SENSOR)){
+			PORTA=PORTA|(1<<LED);			// turn on PA7 and preserve other bits on porta
+		}else{
+			PORTA=PORTA & ~(1<<LED);			// turn off PA7
+		}
+	}
 }
 
